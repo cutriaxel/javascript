96bc197b349fa7b94737c126ce47300f75b2e185
axel cutri <cutriaxel@hotmail.com>
2023-03-28 17:16:53 -0300

    Segunda pre entrega

--- main.js
+++ main.js
@@ -1,67 +1,61 @@
-
 //Bienvenidos a LiezenSHOES!
-alert('Bienvenido a LiezenShoes!!!!!!!')
+alert('Bienvenido a LiezenShoes!!')
+const carrito = []
 
-const comprarProducto = () => {
-    let producto = ''
-    let cantidad = 0
-    let precio = 0
-    let subtotal = 0
-    let seguirComprando = false
+const mostrarProductos = () =>{
+    const listaProductos = productos.map(producto => {
+        return '- '+producto.nombre+' $'+producto.precio
+    })
 
-    do {
-        producto = prompt('¿Querés comprar praga, napoles o Ambos?')
-        cantidad = parseInt(prompt('¿Cuántos queres comprar?'))
-
-        let cantidadValidada = validarCantidad(cantidad)
-
-        switch (producto) {
-            case 'praga':
-                precio = 3000
-                break
-            case 'napoles':
-                precio = 3600
-                break
-            case 'ambos':
-                precio = 6600
-                break
-            default:
-                alert('Alguno de los datos ingresados no es correcto!')
-                precio = 0
-                cantidadValidada = 0
-                break
-        }
-
-        subtotal += precio * cantidadValidada
-
-        seguirComprando = confirm('¿Desea seguir comprando?')
-    } while (seguirComprando)
-
-    return subtotal
+    alert('Lista de productos:'+'\n\n'+listaProductos.join('\n'))
+    comprarProducto()
 }
 
+const comprarProducto = () => {
+    let productoNombre = ''
+    let productoCantidad = 0
+    let seguirComprando =  false
 
-//Validad cantidad
-const validarCantidad = (cantidad) => {
-    while (Number.isNaN(cantidad) || cantidad === 0) {
-        alert('Debe de agregar una cantidad válida!')
-        cantidad = parseInt(prompt('¿Cuántos queres comprar?'))
+    do {
+    productoNombre = prompt('¿Querés comprar praga, napoles o londres?')
+    productoCantidad = parseInt(prompt('¿Cuantos queres comprar?'))
+
+    const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
+
+    if (producto){
+        agregarAlCarrito(producto, producto.id, productoCantidad, producto.precio)
+    }else{
+        alert('El producto no se encuentra en stock!')
     }
+    seguirComprando = confirm('¿Desea agregar otro producto?')
+    } while (seguirComprando);
+    calcularSubtotal()
+}
 
-    return cantidad
-};
-
-
-//Descuentos
-const aplicarDescuento = (subtotal) => {
-    const descuento = 0.80
-
-    if (subtotal >= 20000) {
-        return subtotal * descuento
+const agregarAlCarrito = (producto,productoId, productoCantidad, precio) => {
+    let subtotal = 0
+    const productoRepetido = carrito.find(producto => producto.id == productoId)
+    if (productoRepetido) {
+        productoRepetido.cantidad += productoCantidad
+        subtotal += precio * productoCantidad
     } else {
-        return subtotal
+        producto.cantidad += productoCantidad
+        carrito.push(producto)
     }
+    console.log(carrito)
+
 };
+
+const calcularSubtotal = () => {
+    let subtotal = 0
+    carrito.forEach(producto => {
+        subtotal += producto.cantidad  * producto.precio
+    })
+
+
+    alert("Subtotal: " + subtotal)
+    calcularEnvio(subtotal)
+}
 
 //Envios
 const calcularEnvio = (subtotal) => {
@@ -69,24 +63,12 @@ const calcularEnvio = (subtotal) => {
 
     if (quiereEnvio && subtotal >= 20000) {
         alert('Tenés envio gratis. El total de la compra es: $'+subtotal)
-    } else if (quiereEnvio && subtotal < 4000) {
+    } else {
         subtotal += 1000
         alert('El envío cuesta $1000. El total mas envío es: $'+subtotal)
-    } else {
-        alert('El total de tu compra es: $'+subtotal)
     }
 
-    return subtotal
+    alert('El total a pagar es de  $'+subtotal+'. Gracias por su compra! LiezenShoes')
 };
 
-const mostrarDetalleDeCompra = (precioFinal) => {
-    alert('El total a pagar es de  $'+precioFinal+'. Gracias por su compra! LiezenShoes')
-};
-
-const subtotal = comprarProducto()
-
-const subtotalConDescuento = aplicarDescuento(subtotal)
-
-const precioFinal = calcularEnvio(subtotalConDescuento)
-
-mostrarDetalleDeCompra(precioFinal)
+mostrarProductos()
---
+++ stock.js
@@ -0,0 +1,23 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Praga",
+        precio: 3000,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Napoles",
+        precio: 3600,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Londres",
+        precio: 3800,
+        cantidad:0
+    }
+]
